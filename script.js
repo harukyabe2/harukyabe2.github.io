ac44e4f0aa30460112f452f28ea48579100d48ff
harukyabe2 <kakarotto.3662.dragon@gmail.com>
2024-06-28 12:37:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// script.js
-
 document.addEventListener('DOMContentLoaded', (event) => {
     var modal = document.getElementById("modal");
     var modalImg = document.getElementById("modal-image");
@@ -25,3 +23,4 @@ document.addEventListener('DOMContentLoaded', (event) => {
         }
     }
 });
+
